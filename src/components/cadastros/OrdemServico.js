c612c5c64ff029c51c7c3848ae2642499b4dbd44
Thiago Henrique <thiguin11@gmail.com>
2023-01-19 19:21:49 -0300

    att

--- src/components/cadastros/OrdemServico.js
+++ src/components/cadastros/OrdemServico.js
@@ -1,5 +1,5 @@
 import styles from '../css/Solicita.module.css'
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function OrdemServico() {
     return (
@@ -7,58 +7,58 @@ function OrdemServico() {
             <div className={styles.solicitaContainer}>
                 <h1>Solicitação de Serviço</h1>
                 <form>
-                        <div className={styles.solicitaForm}>
-                            <div>
-                                <label htmlFor="order_description">Descrição </label>
-                                <input
-                                    type="text"
-                                    id="order_description"
-                                    name="order_description"
-                                    placeholder="Digite sua solicitação"/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="order_clientname">Cliente </label>
-                                <input
-                                    type="text"
-                                    id="order_clientname"
-                                    name="order_clientname"
-                                    placeholder="Digite o nome do cliente"/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="order_productname">Produto </label>
-                                <input
-                                    type="text"
-                                    id="order_productname"
-                                    name="order_productname"
-                                    placeholder="Digite o nome do produto"/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="order_service">Serviço </label>
-                                <input
-                                    type="text"
-                                    id="order_service"
-                                    name="order_service"
-                                    placeholder="Digite o nome do produto"/>
-                            </div>
-
-                            <div>
-                                <label htmlFor="order_category">Categoria do Produto </label>
-                                <select name="order_category" id="order_category" required>
-                                    <option value="" selected disabled hidden>Escolha a categoria</option>
-                                    <option value="joias">Joias</option>
-                                    <option value="veiculos">Veículos</option>
-                                    <option value="equipamentos">Equipamentos</option>
-                                    <option value="residencial">Residencial</option>
-                                </select>
-                            </div>
+                    <div className={styles.solicitaForm}>
+                        <div>
+                            <label htmlFor="order_description">Descrição </label>
+                            <input
+                                type="text"
+                                id="order_description"
+                                name="order_description"
+                                placeholder="Digite sua solicitação"/>
                         </div>
 
-                        <div className={styles.solicitaBotao}>
-                            <input type="submit" value="Cadastrar" />
+                        <div>
+                            <label htmlFor="order_clientname">Cliente </label>
+                            <input
+                                type="text"
+                                id="order_clientname"
+                                name="order_clientname"
+                                placeholder="Digite o nome do cliente"/>
                         </div>
+
+                        <div>
+                            <label htmlFor="order_productname">Produto </label>
+                            <input
+                                type="text"
+                                id="order_productname"
+                                name="order_productname"
+                                placeholder="Digite o nome do produto"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="order_service">Serviço </label>
+                            <input
+                                type="text"
+                                id="order_service"
+                                name="order_service"
+                                placeholder="Digite o nome do produto"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="order_category">Categoria do Produto </label>
+                            <select name="order_category" id="order_category" required>
+                                <option value="" selected disabled hidden>Escolha a categoria</option>
+                                <option value="joias">Joias</option>
+                                <option value="veiculos">Veículos</option>
+                                <option value="equipamentos">Equipamentos</option>
+                                <option value="residencial">Residencial</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className={styles.solicitaBotao}>
+                        <input type="submit" value="Solicitar" />
+                    </div>
                 </form>    
             </div>
             
